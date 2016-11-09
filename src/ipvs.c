9d25c9f31793581c89857d67a85d1a611b4e92db
bencabot <cabotben@gmail.com>
2016-11-09 15:53:58 +0000

    Adding policys, removing unnecessary policy and libipvs includes.W

--- src/ipvs.c
+++ src/ipvs.c
@@ -43,33 +43,13 @@
 #endif /* HAVE_NETINET_IN_H */
 
 
-//TODO ben remove this
-// Need to check valid versions to use and what version is int he kernel header.
-//out older version does not have netlink
-//#include "ip_vs.h"
-
-/* this can probably only be found in the kernel sources */
-//#if HAVE_LINUX_IP_VS_H
-//# include <linux/ip_vs.h>
-//#elif HAVE_NET_IP_VS_H
-//# include <net/ip_vs.h>
-//#elif HAVE_IP_VS_H
-//# include <ip_vs.h>
-//#endif /* HAVE_IP_VS_H */
-
-//TODO ben update configure.ac to check netlink version
-// The newer version of netlink install to a differnt path so need to
-// account for this
-
 #include <netlink/netlink.h>
 #include <netlink/genl/genl.h>
 #include <netlink/genl/ctrl.h>
 #include <netlink/msg.h>
-//TODO ben need to decide if we need this file or can just include needed policy
-//#include "ip_vs_nl_policy.c"
 
+//Include our own version of ip_vs.h
 #include "ip_vs.h"
-#include "ip_vs_nl_policy.c"
 
 
 #ifdef LIBIPVS_USE_NL
@@ -80,6 +60,8 @@
 #endif
 static  struct nl_sock *sock = NULL;
 static  int family, try_nl = 1;
+typedef struct ip_vs_service_entry      ipvs_service_entry_t;
+typedef struct ip_vs_dest_entry         ipvs_dest_entry_t;
 #endif
 
 
@@ -93,6 +75,66 @@ static int sockfd = -1;
 static void* ipvs_func = NULL;
 struct ip_vs_getinfo ipvs_info;
 
+#ifdef LIBIPVS_USE_NL
+/* Policy definitions */
+struct nla_policy ipvs_cmd_policy[IPVS_CMD_ATTR_MAX + 1] = {
+	[IPVS_CMD_ATTR_SERVICE]		= { .type = NLA_NESTED },
+	[IPVS_CMD_ATTR_DEST]		= { .type = NLA_NESTED },
+	[IPVS_CMD_ATTR_DAEMON]		= { .type = NLA_NESTED },
+	[IPVS_CMD_ATTR_TIMEOUT_TCP]	= { .type = NLA_U32 },
+	[IPVS_CMD_ATTR_TIMEOUT_TCP_FIN]	= { .type = NLA_U32 },
+	[IPVS_CMD_ATTR_TIMEOUT_UDP]	= { .type = NLA_U32 },
+};
+
+struct nla_policy ipvs_service_policy[IPVS_SVC_ATTR_MAX + 1] = {
+	[IPVS_SVC_ATTR_AF]		= { .type = NLA_U16 },
+	[IPVS_SVC_ATTR_PROTOCOL]	= { .type = NLA_U16 },
+	[IPVS_SVC_ATTR_ADDR]		= { .type = NLA_UNSPEC,
+					    .maxlen = sizeof(struct in6_addr) },
+	[IPVS_SVC_ATTR_PORT]		= { .type = NLA_U16 },
+	[IPVS_SVC_ATTR_FWMARK]		= { .type = NLA_U32 },
+	[IPVS_SVC_ATTR_SCHED_NAME]	= { .type = NLA_STRING,
+					    .maxlen = IP_VS_SCHEDNAME_MAXLEN },
+	[IPVS_SVC_ATTR_FLAGS]		= { .type = NLA_UNSPEC,
+					    .minlen = sizeof(struct ip_vs_flags),
+					    .maxlen = sizeof(struct ip_vs_flags) },
+	[IPVS_SVC_ATTR_TIMEOUT]		= { .type = NLA_U32 },
+	[IPVS_SVC_ATTR_NETMASK]		= { .type = NLA_U32 },
+	[IPVS_SVC_ATTR_STATS]		= { .type = NLA_NESTED },
+};
+
+struct nla_policy ipvs_dest_policy[IPVS_DEST_ATTR_MAX + 1] = {
+	[IPVS_DEST_ATTR_ADDR]		= { .type = NLA_UNSPEC,
+					    .maxlen = sizeof(struct in6_addr) },
+	[IPVS_DEST_ATTR_PORT]		= { .type = NLA_U16 },
+	[IPVS_DEST_ATTR_FWD_METHOD]	= { .type = NLA_U32 },
+	[IPVS_DEST_ATTR_WEIGHT]		= { .type = NLA_U32 },
+	[IPVS_DEST_ATTR_U_THRESH]	= { .type = NLA_U32 },
+	[IPVS_DEST_ATTR_L_THRESH]	= { .type = NLA_U32 },
+	[IPVS_DEST_ATTR_ACTIVE_CONNS]	= { .type = NLA_U32 },
+	[IPVS_DEST_ATTR_INACT_CONNS]	= { .type = NLA_U32 },
+	[IPVS_DEST_ATTR_PERSIST_CONNS]	= { .type = NLA_U32 },
+	[IPVS_DEST_ATTR_STATS]		= { .type = NLA_NESTED },
+};
+
+struct nla_policy ipvs_stats_policy[IPVS_STATS_ATTR_MAX + 1] = {
+	[IPVS_STATS_ATTR_CONNS]		= { .type = NLA_U32 },
+	[IPVS_STATS_ATTR_INPKTS]	= { .type = NLA_U32 },
+	[IPVS_STATS_ATTR_OUTPKTS]	= { .type = NLA_U32 },
+	[IPVS_STATS_ATTR_INBYTES]	= { .type = NLA_U64 },
+	[IPVS_STATS_ATTR_OUTBYTES]	= { .type = NLA_U64 },
+	[IPVS_STATS_ATTR_CPS]		= { .type = NLA_U32 },
+	[IPVS_STATS_ATTR_INPPS]		= { .type = NLA_U32 },
+	[IPVS_STATS_ATTR_OUTPPS]	= { .type = NLA_U32 },
+	[IPVS_STATS_ATTR_INBPS]		= { .type = NLA_U32 },
+	[IPVS_STATS_ATTR_OUTBPS]	= { .type = NLA_U32 },
+};
+
+struct nla_policy ipvs_info_policy[IPVS_INFO_ATTR_MAX + 1] = {
+	[IPVS_INFO_ATTR_VERSION]	= { .type = NLA_U32 },
+	[IPVS_INFO_ATTR_CONN_TAB_SIZE]	= { .type = NLA_U32 },
+};
+#endif
 /*
  * libipvs API
  */
